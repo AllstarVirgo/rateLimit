8d0a72509913be3e76e90cad3143011481802958
wangyongxing <wangyongxing@idea.edu.cn>
2022-06-09 23:50:56 +0800

    add comment

--- src/main/resources/lua/rateLimiter.lua
+++ src/main/resources/lua/rateLimiter.lua
@@ -1,10 +1,16 @@
 local halfDayTimeSeconds = 12 * 60 * 60
 
+--计算在特定时间间隔内需要添加多少个令牌
 local function getAddTokenCount(interval, maxRate)
     local tokenNumberPerMilliseconds = 1000 / maxRate;
     return interval / tokenNumberPerMilliseconds;
 end
 
+--key 唯一标识
+--currentTimeMillis 当前时间戳
+--maxRate 限制速率 qps
+--requestTokenNumber  请求的令牌数
+--返回布尔值,true表示可以拿到令牌，false表示不能拿到令牌
 local function tryAcquire(key, currentTimeMillis, maxRate, requestTokenNumber)
     currentTimeMillis = tonumber(currentTimeMillis)
     maxRate = tonumber(maxRate)
